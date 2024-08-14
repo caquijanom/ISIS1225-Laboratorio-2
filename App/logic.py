889c755487902814cb7caf91fc8a543043279190
Andrés Rodriguez <af.rodriguezm1@uniandes.edu.co>
2024-08-14 01:43:32 -0500

    Laboatorio 2 Commit

---
+++ App/logic.py
@@ -0,0 +1,156 @@
+"""
+ * Copyright 2020, Departamento de sistemas y Computación,
+ * Universidad de Los Andes
+ *
+ *
+ * Desarrolado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along withthis program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contribuciones:
+ *
+ * Dario Correal - Version inicial
+ * Santiago Arteaga - Otras versiones
+ * Andres Rodriguez - Última version
+ """
+
+import csv
+import os
+
+from DataStructures import Set as set
+
+data_dir = os.path.dirname(os.path.realpath('__file__')) + '/Data/'
+
+"""
+La logica es el sitio donde se manipulan los datos y donde se le responde a la view
+"""
+
+
+def new_logic():
+    """
+    Crea una instancia de la logica
+    """
+
+    catalog = {"books": None,
+            "tags": None,
+            "book_tags": None}
+    
+    # Se crean los conjuntos vacios
+    catalog["books"] = set.new_set()
+    catalog["tags"] = set.new_set()
+    catalog["book_tags"] = set.new_set()
+    return catalog
+
+
+# Funciones para la carga de datos
+
+def load_books(catalog, filename):
+    """
+    Carga los libros del archivo.  Por cada libro se toman sus autores y por
+    cada uno de ellos, se crea el conjunto de autores, a dicho autor y una
+    referencia al libro que se esta procesando.
+    """
+    books = catalog["books"]
+    books_file = os.path.join(data_dir, filename)
+    catalog["books"] = set.load_set(books, books_file) 
+    return book_size(catalog)
+
+
+def load_tags(catalog, filename):
+    """
+    Carga todos los tags del archivo y los agrega a la lista de tags
+    """
+    tags_file = os.path.join(data_dir, filename)
+    input_file = csv.DictReader(open(tags_file, encoding="utf-8"))
+    catalog["tags"] = set.new_set()
+    for tag in input_file:
+        set.add_element(catalog["tags"], tag)
+    return tag_size(catalog)
+
+
+def load_books_tags(catalog, filename):
+    """
+    Carga los tags de los libros del archivo
+    """
+    # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
+    pass
+
+
+def first_book(catalog):
+    """
+    Devuelve el primer libro del catalogo
+    """
+    # TODO: Mods Est-3 en el Lab 2
+    pass
+
+
+def last_book(catalog):
+    """
+    Devuelve el ultimo libro del catalogo
+    """
+    # TODO: Mods Est-3 en el Lab 2
+    pass
+
+# Funciones para la manipulacion de los datos 
+
+def add_book_tags(catalog, booktagsfile):
+    """
+    Esta funcion guardar los booktags provenientes del archivo CSV.
+    """
+    # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
+    pass
+
+
+def create_book_tag_list(catalog):
+    """
+    Esta funcion crea una lista vacia para booktags.
+    """
+    # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
+    pass
+
+
+def add_book_tag(catalog, booktag):
+    """
+    Esta funcion agrega un elemento a lista de booktags.
+    """
+    # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
+    pass
+
+
+# Funciones de consulta
+
+def book_size(catalog):
+    # TODO: Mods de Est-3 en el Lab 2
+    pass
+
+
+def tag_size(catalog):
+    # TODO: Mods de Est-3 en el Lab 2
+    pass
+
+
+def book_tag_size(catalog):
+    # TODO: Mods de Est-3 en el Lab 2
+    pass
+
+
+def first_book(catalog):
+    # TODO: Mods de Est-3 en el Lab 2
+    pass
+
+
+def last_book(catalog):
+    # TODO: Mods de Est-3 en el Lab 2
+    pass
---
+++ App/view.py
@@ -0,0 +1,138 @@
+"""
+ * Copyright 2020, Departamento de sistemas y Computación,
+ * Universidad de Los Andes
+ *
+ *
+ * Desarrolado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along withthis program.  If not, see <http://www.gnu.org/licenses/>.
+ * contribuciones:
+ *
+ * Dario Correal - Version inicial
+ * Santiago Arteaga - Otras versiones
+ * Andres Rodriguez - Última version
+ """
+
+import sys
+import App.logic as logic
+
+
+"""
+La vista se encarga de la interacción con el usuario
+Presenta el menu de opciones  y  por cada seleccion
+se hace la solicitud al controlador para ejecutar la
+operación solicitada
+"""
+
+
+def new_logic():
+    """
+    Se crea una instancia de la logica
+    """
+    control = logic.new_logic()
+    return control
+
+
+def print_menu():
+    print("Opciones:")
+    print("1- Cargar Libros")
+    print("2- Cargar Tags")
+    # TODO: Mods de Est-1 en el Lab 2, agregar opcion 3
+
+    print("0- Salir")
+
+
+def load_books(control):
+    """
+    Carga los libros
+    """
+    books = logic.load_books(control,
+                                  "GoodReads/books-small.csv")
+    return books
+
+
+def load_tags(control):
+    """
+    Carga los Tags
+    """
+    tags = logic.load_tags(control,
+                                "GoodReads/tags.csv")
+    return tags
+
+
+def load_books_tags(control):
+    """
+    Cargar los Tags de libros
+    """
+    # TODO: Mods de Est-1 en el Lab 2
+    pass
+
+
+def first_book(control):
+    """
+    Devuelve el primer libro del catalogo
+    """
+    # TODO: Mods de Est-1 en el Lab 2
+    pass
+
+
+def last_book(control):
+    # TODO: Mods de Est-2 en el Lab 2
+    """
+    Devuelve el último libro cargado
+    """
+    pass
+
+
+# Se crea el controlador asociado a la vista
+control = new_logic()
+
+# main del ejercicio
+def main():
+    """
+    Menu principal
+    """
+    working = True
+    # ciclo del menu
+    while working:
+        print_menu()
+        inputs = input("Seleccione una opción para continuar\n")
+        if int(inputs[0]) == 1:
+            print("Cargando información de libros....")
+            books = load_books(control)
+            print("Total de libros cargados: " + str(books) + "\n")
+
+            # TODO: Mods de Est-1 en el Lab 2
+            first = None
+
+            # TODO: Mods de Est-2 en el Lab 2
+            last = None
+
+        elif int(inputs[0]) == 2:
+            print("Cargando información de tags....")
+            tags = load_tags(control)
+            print("Total de tags cargados: " + str(tags) + "\n")
+
+        elif int(inputs[0]) == 3:
+            # TODO: Mods de Est-3 en el Lab 2
+            pass
+
+        elif int(inputs[0]) == 0:
+            working = False
+            print("\nGracias por utilizar el programa.")
+
+        else:
+            print("Opcion erronea, vuelva a elegir.\n")
+    sys.exit(0)
